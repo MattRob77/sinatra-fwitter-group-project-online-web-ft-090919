25d76f8f976988fad8624e4b7718d2e623cc2c53
Matthew Robinson <matt_robinson1@yahoo.com>
2019-10-24 23:17:04 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ActiveRecord::Base
-  has_secure_password
   has_many :tweets
-  validates :username, presence: true
-  validates :email, presence: true
+
+
+  has_secure_password
 
   def slug
-    self.username.downcase.gsub(" ", "-")
+    username.downcase.gsub(' ', '-')
   end
+
+  def self.find_by_slug(slug)
+    User.all.find { |user| user.slug == slug }
+  end
+
 end
