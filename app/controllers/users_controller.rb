a8c490cf6347412bb47c557c6669e425e6c3094c
Matthew Robinson <matt_robinson1@yahoo.com>
2019-10-24 23:15:53 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,24 @@
 class UsersController < ApplicationController
-
-  get '/users' do
-
-    erb :index
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
   end
 
   get '/signup' do
     if !logged_in?
-      erb :'users/signup'
+      erb :'users/create_user', locals: {message: 'Please sign up before you sign in'}
     else
+      redirect to '/tweets'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == '' || params[:email] == '' || params[:password] == ''
+      redirect to '/signup'
+    else
+      @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      @user.save
+      session[:user_id] = @user.id
       redirect to '/tweets'
     end
   end
@@ -17,50 +27,26 @@ class UsersController < ApplicationController
     if !logged_in?
       erb :'users/login'
     else
-      redirect to('/tweets')
+      redirect to '/tweets'
     end
-
   end
 
   post '/login' do
     user = User.find_by(username: params[:username])
-
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-
-      redirect to('/tweets')
+      redirect to '/tweets'
     else
-      redirect to('/login')
+      redirect to '/signup'
     end
-  end
-
-
-  post '/signup' do
-
-    user = User.new(params)
-
-    if user.save
-      session[:user_id] = user.id
-      redirect to('/tweets')
-    else
-      redirect to('/signup')
-    end
-
   end
 
   get '/logout' do
-    # logged_in? ? (erb :'users/logout') : (redirect to('/'))
     if logged_in?
       session.destroy
-      redirect to('/login')
+      redirect to '/login'
     else
-      redirect to('/')
+      redirect to '/'
     end
   end
-
-  get '/users/:slug' do
-   @user = User.find_by_slug(params[:slug])
-   erb :'users/show'
- end
-
 end
